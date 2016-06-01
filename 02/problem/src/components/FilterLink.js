d56ba8fc7a3a13f348e89186c21b43ea42eed515
liormb <liormb@gmail.com>
2016-06-01 11:45:44 +0200

    refactor solution 02

--- 02/problem/src/components/FilterLink.js
+++ 02/problem/src/components/FilterLink.js
@@ -1,20 +1,26 @@
-import { connect } from 'react-redux';
-import { setVisibilityFilter } from '../actions';
-import Link from './Link';
 
-const mapStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.visibilityFilter,
-});
+'use strict';
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick() {
-    dispatch(setVisibilityFilter(ownProps.filter));
-  },
-});
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 
-const FilterLink = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Link);
+const FilterLink = ({ filter, children }) => (
+    <Link
+        to={filter === 'all' ? '' : filter}
+        activeStyle={{
+            textDecoration: 'none',
+            color: 'black'
+        }}
+    >
+        {children}
+    </Link>
+);
+
+FilterLink.propTypes = {
+    filter: PropTypes.oneOf([
+        'all', 'active', 'completed'
+    ]).isRequired,
+    children: PropTypes.node.isRequired
+};
 
 export default FilterLink;
--- 02/problem/src/components/Root.js
+++ 02/problem/src/components/Root.js
@@ -1,9 +1,21 @@
-/**
- * User: Lior Elrom
- * Date: 06/01/16
- * Time: 11:12 AM
- */
 
 'use strict';
 
+import React, { PropTypes } from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import App from './App';
 
+const Root = ({ store }) => (
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/(:filter)" component={App} />
+        </Router>
+    </Provider>
+);
+
+Root.propTypes = {
+    store: PropTypes.object.isRequired
+};
+
+export default Root;
